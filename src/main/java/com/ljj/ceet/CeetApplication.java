180b82b22e2609335db15a685139c2be8e065f97
kanateSong <545430154@qq.com>
2019-04-15 21:57:35 +0800

    人事管理系统

--- src/main/java/com/ljj/ceet/CeetApplication.java
+++ src/main/java/com/ljj/ceet/CeetApplication.java
@@ -4,11 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan
-@ComponentScan(basePackages = { "org.n3r.idworker"})
+//@ComponentScan(basePackages = { "org.n3r.idworker"})
 //@MapperScan("com.ljj.ceet.mapper")
 public class CeetApplication {
 
--- src/main/java/com/ljj/ceet/controller/CenterController.java
+++ src/main/java/com/ljj/ceet/controller/CenterController.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -55,15 +54,12 @@ public class CenterController extends BaseController{
 
     }
 
-    @GetMapping(value = "a")
+    @RequestMapping("/a")
     public String test(){
+        log.info("aaa");
         return "index";
     }
 
-    @GetMapping(value = "/a")
-    public String test1(){
-        return "index";
-    }
 
     /*@GetMapping(value = "/a")
     public String test2(){
